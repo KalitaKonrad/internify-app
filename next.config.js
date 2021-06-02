2af1991c1cee7f1a891cdb6a90769a2c678ee7b2
Konrad <konradkalita@student.agh.edu.pl>
2021-06-02 12:06:18 +0200

    feat: cleanup code, adjust prettier, format all files to 2 spaces indentation

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withPlugins = require("next-compose-plugins");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextTranslate = require("next-translate");
+
+const config = {
+  future: {
+    webpack5: true,
+  },
+};
+
+module.exports = withPlugins([[withBundleAnalyzer], [nextTranslate]], config);
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+
+const Home: React.FC = () => {
+  return (
+    <Flex direction="column" minH="100vh">
+      Witam hehe
+    </Flex>
+  );
+};
+
+export default Home;
